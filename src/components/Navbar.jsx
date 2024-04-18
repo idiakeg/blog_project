7db160685a28c641e5b3ccb7c75ab7641cbd1bd1
idiakeg <idiakeg@gmail.com>
2024-04-18 17:06:47 +0100

    Login functionality implementation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,57 +1,83 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../mern-blog-assets-main/logo.png";
 import { FaBars } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
+import { useAuth } from "../context/AuthProvider";
 
 const Navbar = () => {
-  const [isNavShowing, setIsNavShowing] = useState(false);
-  return (
-    <nav>
-      <div className="container nav__container">
-        <Link
-          to="/"
-          className="nav__logo"
-          onClick={() => setIsNavShowing(false)}
-        >
-          <img src={logo} alt="nav logo" />
-        </Link>
-        <ul className={`nav__menu ${isNavShowing ? "show__nav" : "hide__nav"}`}>
-          <li>
-            <Link
-              to="/profile/profile_id"
-              onClick={() => setIsNavShowing((prev) => !prev)}
-            >
-              Ernest Achiever
-            </Link>
-          </li>
-          <li>
-            <Link to="/create" onClick={() => setIsNavShowing((prev) => !prev)}>
-              Create Post
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/authors"
-              onClick={() => setIsNavShowing((prev) => !prev)}
-            >
-              Authors
-            </Link>
-          </li>
-          <li>
-            <Link to="/logout" onClick={() => setIsNavShowing((prev) => !prev)}>
-              Logout
-            </Link>
-          </li>
-        </ul>
-        <button
-          className="nav__toggle-btn"
-          onClick={() => setIsNavShowing((prev) => !prev)}
-        >
-          {isNavShowing ? <AiOutlineClose /> : <FaBars />}
-        </button>
-      </div>
-    </nav>
-  );
+    const { handleLogout } = useAuth();
+    const [isNavShowing, setIsNavShowing] = useState(false);
+
+    let appUser = JSON.parse(localStorage.getItem("bird_app_user"));
+
+    const handleLogoutClick = () => {
+        setIsNavShowing((prev) => !prev);
+        handleLogout();
+    };
+
+    return (
+        <nav>
+            <div className="container nav__container">
+                <Link
+                    to="/"
+                    className="nav__logo"
+                    onClick={() => setIsNavShowing(false)}
+                >
+                    <img src={logo} alt="nav logo" />
+                </Link>
+                <ul
+                    className={`nav__menu ${
+                        isNavShowing ? "show__nav" : "hide__nav"
+                    }`}
+                >
+                    {appUser && (
+                        <>
+                            <li>
+                                <Link
+                                    to="/profile/profile_id"
+                                    onClick={() =>
+                                        setIsNavShowing((prev) => !prev)
+                                    }
+                                >
+                                    {appUser?.name}
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to="/create"
+                                    onClick={() =>
+                                        setIsNavShowing((prev) => !prev)
+                                    }
+                                >
+                                    Create Post
+                                </Link>
+                            </li>
+                        </>
+                    )}
+
+                    <li>
+                        <Link
+                            to="/authors"
+                            onClick={() => setIsNavShowing((prev) => !prev)}
+                        >
+                            Authors
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/login" onClick={handleLogoutClick}>
+                            {appUser ? "Logout" : "Login"}
+                        </Link>
+                    </li>
+                </ul>
+                <button
+                    className="nav__toggle-btn"
+                    onClick={() => setIsNavShowing((prev) => !prev)}
+                >
+                    {isNavShowing ? <AiOutlineClose /> : <FaBars />}
+                </button>
+            </div>
+        </nav>
+    );
 };
 export default Navbar;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,52 +1,76 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthProvider";
+import Loading from "../components/Loading/Loading";
 
 const Login = () => {
-  const [userData, setUserData] = useState({
-    email: "",
-    password: "",
-  });
-
-  const handleInputChange = (e) => {
-    setUserData((prevState) => {
-      return {
-        ...prevState,
-        [e.target.name]: e.target.value,
-      };
+    const { handleLogin, isLoading, errorMessage } = useAuth();
+    const [userData, setUserData] = useState({
+        email: "",
+        password: "",
     });
-  };
 
-  return (
-    <section className="login">
-      <div className="container login__container">
-        <h2>Sign In</h2>
-        <form action="" className="form login__form">
-          <p className="form__error-message">This is an error message</p>
-          <input
-            type="email"
-            placeholder="Email"
-            name="email"
-            value={userData.email}
-            onChange={handleInputChange}
-            autoFocus
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={userData.password}
-            onChange={handleInputChange}
-          />
-          <button type="submit" className="btn primary">
-            Login
-          </button>
-        </form>
-        <small>
-          Don't have an account? <Link to={`/register`}>sign up </Link>
-        </small>
-      </div>
-    </section>
-  );
+    const handleInputChange = (e) => {
+        setUserData((prevState) => {
+            return {
+                ...prevState,
+                [e.target.name]: e.target.value,
+            };
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        handleLogin(userData.email, userData.password);
+    };
+
+    return (
+        <>
+            {isLoading ? (
+                <Loading />
+            ) : (
+                <section className="login">
+                    <div className="container login__container">
+                        <h2>Sign In</h2>
+                        <form
+                            onSubmit={handleSubmit}
+                            className="form login__form"
+                        >
+                            {errorMessage && (
+                                <p className="form__error-message">
+                                    {errorMessage}
+                                </p>
+                            )}
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                name="email"
+                                value={userData.email}
+                                onChange={handleInputChange}
+                                autoFocus
+                                required
+                            />
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                name="password"
+                                value={userData.password}
+                                onChange={handleInputChange}
+                                required
+                            />
+                            <button type="submit" className="btn primary">
+                                Login
+                            </button>
+                        </form>
+                        <small>
+                            Don't have an account?{" "}
+                            <Link to={`/register`}>sign up </Link>
+                        </small>
+                    </div>
+                </section>
+            )}
+        </>
+    );
 };
 
 export default Login;
